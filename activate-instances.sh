c74705d348922901babc2186566f22f9a6e8e349
Nazay <121894384+nazayb@users.noreply.github.com>
2024-11-16 16:11:07 +0100

    Update activate-instances.sh

--- activate-instances.sh
+++ activate-instances.sh
@@ -1,7 +1,7 @@
 
 gcloud compute instances list
 # loop thhrought INSTANCES_NAMES List and stop the instances
-INSTANCES_NAMES=(c3-standard-4 c4-standard-4 n4-standard-4 e2-standard-4)
+INSTANCES_NAMES=(c3-standard-4 c4-standard-4 n4-standard-4)
 for VM_NAME in ${INSTANCES_NAMES[*]}
 do
   gcloud compute instances start $VM_NAME 
